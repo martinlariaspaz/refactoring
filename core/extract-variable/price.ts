81f3c484971349104d2e1be9531a7312c77ef7b4
Martin L. Arias Paz <martinlariaspaz@gmail.com>
2025-08-03 13:09:36 -0300

    refactor: "extract variable with test"

--- core/extract-variable/price.ts
+++ core/extract-variable/price.ts
@@ -14,11 +14,12 @@ import { Order } from "./types";
  */
 
 function price(order: Order) {
-  return (
-    order.quantity * order.itemPrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100)
-  );
+  const basePrice = order.quantity * order.itemPrice;
+  const quantityDiscount =
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const shippingPrice = Math.min(basePrice * 0.1, 100);
+
+  return basePrice - quantityDiscount + shippingPrice;
 }
 
 export default price;
